acca36ead433f34b29379a74322a2b3d1f7df256
Nuch <ichranuch@gmail.com>
2025-12-30 11:46:11 +0700

    Fix Dashboard import and update project files

--- src/main.jsx
+++ src/main.jsx
@@ -4,24 +4,21 @@ import './index.css'
 import App from './App.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RegisterPage from './Register.jsx';
-import Dashboard from "./Dashboard.jsx";
 import { AuthProvider } from './Auth/authContext.jsx';
 import TaskPage from './createEditTask.jsx';
+import DashboardPage from './Dashboard.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode> 
     <AuthProvider>
       <BrowserRouter>
-      {/* Routes */}
-      <Routes>
-        <Route path="/" element={ <App />} />
-        < Route path="/register" element={ <RegisterPage />} />
-        <Route path="/dashboard" element={ <DashboardPage />} />
-        <Route path="/task" element={ <TaskPage />} />
-        {/* <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} /> */}
-      </Routes>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/dashboard" element={<DashboardPage />} />
+          <Route path="/task" element={<TaskPage />} />
+        </Routes>
       </BrowserRouter>
     </AuthProvider>
-  </StrictMode>,
+  </StrictMode>
 )
